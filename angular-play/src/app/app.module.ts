33484e7b9fbbceb9b52a290deb900863778ff9f1
avm-ru-cs <avm@ryerson.ca>
2019-03-27 22:09:17 -0400

    Create component About

--- angular-play/src/app/app.module.ts
+++ angular-play/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
