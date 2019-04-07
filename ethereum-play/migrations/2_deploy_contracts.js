ddf012978303a89dfac3535d8d174462c6273b3e
Andriy Miranskyy <miranska@yahoo.com>
2019-04-07 16:45:11 -0400

    Create a contract storing multiple documents

--- ethereum-play/migrations/2_deploy_contracts.js
+++ ethereum-play/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 // the file is created manually
 // the file is created manually
 var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
 module.exports = function(deployer) {
   deployer.deploy(ProofOfExistence1);
+  deployer.deploy(ProofOfExistence2);
 };
 
